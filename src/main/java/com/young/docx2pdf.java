141093259b61648ad12f88decd7bdfacf3ba1af0
durti <durti@DESKTOP-KYHONG>
2026-01-19 10:59:28 +0900

    test1

--- src/main/java/com/young/docx2pdf.java
+++ src/main/java/com/young/docx2pdf.java
@@ -49,6 +49,8 @@ public class docx2pdf {
 	 */
 	public File toPDF(String inputPath, String outputPath , String xconfPath)
 	{
+		System.out.println("자동 배포 테슽.");
+		
 		File newFile = null;
 		
 		OutputStream os = null;
